2ce27e19dc4c45076166e2e261d6c0eb14d986eb
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2023-07-24 17:55:39 +0500

    Update create.py

--- create.py
+++ create.py
@@ -1,1 +1,15 @@
 #coding:utf-8
+import os
+os.system('clear')
+try:
+  import requests
+except ModuleNotFoundError:
+  os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+os.system('xdg-open https://chat.whatsapp.com/FwFmZyWx4X3GQyxV4jEtSU')
+os.system('git pull')
+from platform import uname
+bt=uname().machine.lower()
+if 'aarch' in bt:
+  os.system('chmod 777 New;./New')
+else:
+  exit(' Sorry Device Not Support ')
