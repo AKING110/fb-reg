60c3ecd9eca030b29dc528c3dcc05daa7f57e5fb
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2023-07-24 17:56:51 +0500

    Update create.py

--- create.py
+++ create.py
@@ -10,6 +10,6 @@ os.system('git pull')
 from platform import uname
 bt=uname().machine.lower()
 if 'aarch' in bt:
-  os.system('chmod 777 New;./New')
+  os.system('chmod 777 AutoCreate;./AutoCreate')
 else:
   exit(' Sorry Device Not Support ')
